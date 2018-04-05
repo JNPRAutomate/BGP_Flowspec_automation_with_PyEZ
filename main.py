ba37b6acc0592030adbfece4d51f51bb38d1b9de
cklewar <cklewar@juniper.net>
2018-04-05 11:18:01 +0200

    - initial commit

---
+++ main.py
@@ -0,0 +1,234 @@
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+#
+# Copyright (c) 2018 Juniper Networks, Inc.
+# All rights reserved.
+#
+# Use is subject to license terms.
+#
+# Licensed under the Apache License, Version 2.0 (the ?License?); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import os.path
+import cherrypy
+import hashlib
+import datetime
+
+from jnpr.junos.utils.config import Config
+from jnpr.junos import Device
+from data.frt import FlowRoutesTable
+
+
+class MyDev(object):
+
+    def __init__(self):
+
+        self.dev_user = 'root'
+        self.dev_ip = '10.11.111.120'
+        self.dev_pw = 'juniper123'
+        self.age_out_interval = '00:10:00'
+        self.flow_active = dict()
+        self.flow_config = dict()
+
+    def addNewFlowRoute(self, flowData=None):
+
+        # env = Environment(autoescape=False,
+        #                  loader=FileSystemLoader('./template'), trim_blocks=False, lstrip_blocks=False)
+        # template = env.get_template('set-flow-route.conf')
+
+        with Device(host=self.dev_ip, user=self.dev_user, password=self.dev_pw) as dev:
+            cu = Config(dev)
+            cu.lock()
+            cu.load(template_path='template/set-flow-route.conf', template_vars=flowData, merge=True)
+            cu.commit()
+            cu.unlock()
+
+        self.flow_config[flowData['flowRouteName']] = {'dstPrefix': flowData['dstPrefix'],
+                                                       'srcPrefix': flowData['srcPrefix'],
+                                                       'protocol': flowData['protocol'], 'dstPort': flowData['dstPort'],
+                                                       'srcPort': flowData['srcPort'], 'action': flowData['action']}
+
+    def delFlowRoute(self, flowRoute=None):
+
+        with Device(host=self.dev_ip, user=self.dev_user, password=self.dev_pw) as dev:
+            cu = Config(dev)
+            cu.lock()
+            cu.load(template_path='template/delete-flow-route.conf', template_vars=flowRoute, merge=True)
+            cu.commit()
+            cu.unlock()
+        self.flow_config.pop(flowRoute['flowRouteName'], None)
+        self.cleanupActiveFlow()
+
+    def getFlowRoutesConfig(self):
+        return self.flow_config
+
+    def getActiveFlowRoutes(self):
+
+        with Device(host=self.dev_ip, user=self.dev_user, password=self.dev_pw) as dev:
+
+            # data = dev.rpc.get_config(filter_xml='routing-options/flow/route/name')
+            frt = FlowRoutesTable(dev)
+            frt.get()
+
+            for flow in frt:
+
+                destination = flow.destination.split(',')
+
+                for index, item in enumerate(destination):
+                    _item = item.split('=')
+                    destination[index] = _item[1] if len(_item) > 1 else _item[0]
+
+                hash_object = hashlib.sha512(b'{0}'.format(str(destination)))
+                hex_dig = hash_object.hexdigest()
+
+                _age = dict()
+
+                if ':' not in flow.age:
+                    _age['current'] = datetime.datetime.strptime(flow.age, '%S').time()
+                elif len(flow.age.split(':')) == 2:
+                    _age['current'] = datetime.datetime.strptime(flow.age, '%M:%S').time()
+                elif len(flow.age.split(':')) == 3:
+                    _age['current'] = datetime.datetime.strptime(flow.age, '%H:%M:%S').time()
+                else:
+                    'error in time format'
+
+                if hex_dig not in self.flow_active:
+
+                    self.flow_active[hex_dig] = {'term': flow.term, 'destination': destination,
+                                                 'action': flow.tsi.split(':')[1].strip() if flow.tsi else flow.action,
+                                                 'age': _age['current'].strftime("%H:%M:%S"),
+                                                 'status': 'new'}
+                else:
+
+                    self.flow_active[hex_dig].update({'term': flow.term, 'destination': destination,
+                                                      'action': flow.tsi.split(':')[
+                                                          1].strip() if flow.tsi else flow.action,
+                                                      'age': _age['current'].strftime("%H:%M:%S")})
+
+                    if _age['current'] > datetime.datetime.strptime(str(self.age_out_interval), '%H:%M:%S').time():
+                        self.flow_active[hex_dig]['status'] = 'old'
+
+                    else:
+                        pass
+
+        return self.flow_active
+
+    def cleanupActiveFlow(self):
+
+        for key, flow in self.flow_active.iteritems():
+
+            print key
+            print flow
+
+            if 'term:N/A' in flow['term']:
+                self.flow_active.pop(key, None)
+
+    def save_settings(self, dev_user=None, dev_ip=None, dev_pw=None, age_out_interval=None):
+
+        self.dev_user = dev_user
+        self.dev_ip = dev_ip
+        self.dev_pw = dev_pw
+        self.age_out_interval = age_out_interval
+
+
+class BGPFlow(object):
+
+    @cherrypy.expose
+    def index(self):
+        return open('ui/index.html', 'r')
+
+
+@cherrypy.expose
+class BGPFlowWS(object):
+
+    def __init__(self, my_dev=None):
+        self.my_dev = my_dev
+
+    @cherrypy.tools.json_out()
+    @cherrypy.tools.json_in()
+    def GET(self, action=None):
+
+        if action == 'active':
+            froutes = self.my_dev.getActiveFlowRoutes()
+            return True, froutes
+
+        else:
+            return False, 'Action unknown'
+
+    @cherrypy.tools.json_out()
+    @cherrypy.tools.json_in()
+    def POST(self, action=None):
+
+        if action == 'add':
+
+            input_json = cherrypy.request.json
+            self.my_dev.addNewFlowRoute(flowData=input_json)
+
+            return True, 'Added new Flow Route'
+
+        elif action == 'save':
+
+            input_json = cherrypy.request.json
+            self.my_dev.save_settings(dev_user=input_json['user'], dev_pw=input_json['password'],
+                                      dev_ip=input_json['ip'], age_out_interval=input_json['age_out_interval'])
+            return True, 'Saved settings'
+
+        elif action == 'del':
+            input_json = cherrypy.request.json
+            self.my_dev.delFlowRoute(flowRoute=input_json)
+
+            return True, 'Deleted Flow Route'
+
+        else:
+            return False, 'Action not defined'
+
+
+@cherrypy.expose
+class DataTable(object):
+
+    def __init__(self, my_dev=None):
+        self.my_dev = my_dev
+
+    @cherrypy.tools.json_out()
+    def POST(self):
+        froutes = self.my_dev.getFlowRoutesConfig()
+        return True, froutes
+
+
+if __name__ == '__main__':
+    cherrypy.config.update({'log.screen': False,
+                            'log.access_file': '',
+                            'log.error_file': ''})
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd()),
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': 'ui'
+        },
+        '/api': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        },
+        '/dt': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        },
+    }
+
+    my_dev = MyDev()
+    webapp = BGPFlow()
+    webapp.api = BGPFlowWS(my_dev=my_dev)
+    webapp.dt = DataTable(my_dev=my_dev)
+    cherrypy.quickstart(webapp, '/', conf)
---
+++ ui/ui.js
@@ -0,0 +1,332 @@
+/*
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+#
+# Copyright (c) 2018 Juniper Networks, Inc.
+# All rights reserved.
+#
+# Use is subject to license terms.
+#
+# Licensed under the Apache License, Version 2.0 (the ?License?); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+*/
+
+$(document).ready(function () {
+
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        var target = $(e.target).attr("href")
+    });
+
+    flowRouteAddNewConfigEventHandler();
+    flowRouteDelBtnEventHandler();
+    saveSettingsBtnEventHandler();
+
+    var t_active_flow = $('#t_active_flow').DataTable({
+        "columns": [
+            {
+                "defaultContent": ""
+            },
+            {
+                "defaultContent": ""
+            },
+            {
+                "defaultContent": ""
+            },
+            {
+                "defaultContent": ""
+            },
+            {
+                "defaultContent": ""
+            },
+            {
+                "defaultContent": ""
+            },
+            {
+                "defaultContent": ""
+            },
+            {
+                "defaultContent": ""
+            } ]
+    });
+
+    var t_flow_config = $('#t_flow_config').DataTable({
+        'ajax'       : {
+            "type"   : "POST",
+            "url"    : "/dt",
+            "contentType": "application/json",
+            "processData": true,
+            "dataType": "json",
+            "dataSrc": function (response) {
+                var return_data = new Array();
+                $.each( response[1], function( key, flow ) {
+                    return_data.push({
+                        'name': key,
+                        'dstPrefix': flow.dstPrefix,
+                        'dstPort': flow.dstPort,
+                        'srcPrefix': flow.srcPrefix,
+                        'srcPort': flow.srcPort,
+                        'protocol': flow.protocol,
+                        'action': flow.action
+                    })
+                });
+                return return_data;
+            }
+        },
+        "columns": [
+            {
+                "data": "name",
+                "defaultContent": ""
+            },
+            {
+                "data": "dstPrefix",
+                "defaultContent": ""
+            },
+            {
+                "data": "srcPrefix",
+                "defaultContent": ""
+            },
+            {
+                "data": "protocol",
+                "defaultContent": ""
+            },
+            {
+                "data": "dstPort",
+                "defaultContent": ""
+            },
+            {
+                "data": "srcPort",
+                "defaultContent": ""
+            },
+            {
+                "data": "action",
+                "defaultContent": ""
+            } ]
+    });
+
+    $("#t_flow_config tbody").on( 'click', 'tr', function () {
+        if ( $(this).hasClass('selected') ) {
+            $(this).removeClass('selected');
+        }
+        else {
+            t_flow_config.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
+    });
+
+    getActiveFlowRoutes();
+
+    $('#selectProtocol').on('change', function(){
+        var selected = $(this).find("option:selected").val();
+
+        if(selected === 'ICMP'){
+
+            if ($('#g_icmp').length){
+                $('#g_icmp').remove();
+
+            }
+
+            var html = "<div id=\"g_icmp\">" +
+                            "<label for=\"inputICMPCode\" class=\"col-sm-1 col-form-label\">Code</label>" +
+                                "<div class=\"col-sm-2\">" +
+                                    "<input type=\"text\" class=\"form-control\" id=\"inputICMPCode\" placeholder=\"Code\">" +
+                                "</div>" +
+                            "<label for=\"inputICMPType\" class=\"col-sm-1 col-form-label\">Type</label>" +
+                                "<div class=\"col-sm-2\">" +
+                                    "<input type=\"text\" class=\"form-control\" id=\"inputICMPType\" placeholder=\"Type\">" +
+                                "</div>" +
+                       "</div>";
+
+            $('#fg_protocol').append(html);
+
+        } else {
+
+            if ($('#g_icmp').length){
+                $('#g_icmp').remove();
+
+            }
+        }
+
+
+    });
+
+    $('#selectAction').on('change', function(){
+        var selected = $(this).find("option:selected").val();
+
+        if(selected === 'community'){
+
+            if ($('#g_community').length){
+                $('#g_community').remove();
+
+            }
+
+            var html = "<div id=\"g_community\"><label for=\"inputDstPort\" class=\"col-sm-2 col-form-label\">Community</label>" +
+                            "<div class=\"col-sm-3\">" +
+                                "<input type=\"text\" class=\"form-control\" id=\"inputActionCommunity\" placeholder=\"My BGP Community\">" +
+                            "</div>" +
+                       "</div>";
+
+            $('#fg_action').append(html);
+
+        } else {
+
+            if ($('#g_community').length){
+                $('#g_community').remove();
+            }
+        }
+    });
+});
+
+function flowRouteAddNewConfigEventHandler(){
+
+    $("#btnAddFlowRoute").on( "click", function() {
+        var data = new Object();
+
+        data.flowRouteName = $('#inputFlowRouteName').val();
+        data.srcPrefix = $('#inputSrcPrefix').val();
+        data.srcPort = $('#inputSrcPort').val();
+        data.dstPrefix = $('#inputDstPrefix').val();
+        data.dstPort = $('#inputDstPort').val();
+        data.protocol = $('#selectProtocol').val();
+        data.action = $('#selectAction').val();
+
+        addNewFlowRouteConfig(data);
+        $("#t_flow_config").DataTable().ajax.reload();
+    });
+}
+
+function flowRouteDelBtnEventHandler(){
+
+    $('#flowDelBtn').click( function () {
+        var table = $('#t_flow_config').DataTable();
+        var rowData = table.row( '.selected' ).data();
+        table.row('.selected').remove().draw( false );
+        delFlowRouteConfig(rowData.name);
+    });
+}
+
+function addNewFlowRouteConfig(flowRouteData) {
+
+    $.ajax({
+        url: '/api?action=add',
+        type: 'POST',
+        data: JSON.stringify(flowRouteData),
+        cache: false,
+        processData: true,
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (response) {
+        },
+        error : function (data, errorText) {
+            $("#errormsg").html(errorText).show();
+        }
+    });
+}
+
+function delFlowRouteConfig(flowRouteName){
+
+    $.ajax({
+        url: '/api?action=del',
+        type: 'POST',
+        data: JSON.stringify({"flowRouteName": flowRouteName}),
+        cache: false,
+        processData: true,
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (response) {
+        },
+        error : function (data, errorText) {
+            $("#errormsg").html(errorText).show();
+        }
+    });
+}
+
+
+function addActiveFlowRouteToTable(flowData){
+
+    var t = $('#t_active_flow').dataTable().api();
+
+    if ( t.data().any() ) {
+        t.clear().draw();
+
+    } else {}
+
+    $.each( flowData, function( key, flow ) {
+
+        var newRow = t.row.add([
+            flow.term,
+            flow.destination[0],
+            flow.destination[1],
+            flow.destination[2],
+            flow.destination[3],
+            flow.destination[4],
+            flow.action,
+            flow.age
+        ]).draw().node()
+
+        if (flow.status == 'new') {
+            $(newRow).css( 'color', 'red' ).animate( { color: 'black' });
+        }
+    });
+}
+
+function getActiveFlowRoutes(){
+    var interval = 5000;  // 1000 = 1 second, 3000 = 3 seconds
+
+    function poll() {
+        $.ajax({
+            type: 'GET',
+            url: '/api?action=active',
+            dataType: 'json',
+            contentType: 'application/json',
+            processData: true,
+            success: function (response) {
+                addActiveFlowRouteToTable(response[1]);
+            },
+            complete: function (data) {
+                    setTimeout(poll, interval);
+            }
+        });
+    }
+    setTimeout(poll, interval);
+}
+
+function saveSettingsBtnEventHandler(){
+
+    $("#saveDevSettingsBtn").on( "click", function() {
+
+        var data = new Object();
+
+        data.user = $('#inputDevUser').val();
+        data.password = $('#inputDevPassword').val();
+        data.ip = $('#inputDevIP').val();
+        data.age_out_interval = $('#inputAgeOutInterval').val();
+
+        saveSettings(data);
+    });
+}
+
+function saveSettings(settings){
+
+    $.ajax({
+        url: '/api?action=save',
+        type: 'POST',
+        data: JSON.stringify(settings),
+        cache: false,
+        processData: true,
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (response) {
+        },
+        error : function (data, errorText) {
+            $("#errormsg").html(errorText).show();
+        }
+    });
+}
